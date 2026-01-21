c0e1385b3ce0e93260463bfcbf1dd6342842d6a1
craftablescience <me@craftablescience.info>
2026-01-20 17:30:07 -0800

    fix: build with cmake below 3.30

--- cmake/clone-repo.cmake
+++ cmake/clone-repo.cmake
@@ -1,4 +1,7 @@
-cmake_policy(SET CMP0169 OLD)
+# Calling FetchContent_Populate() with a single argument is deprecated >= 3.30
+if(${CMAKE_VERSION} VERSION_GREATER_EQUAL "3.30")
+    cmake_policy(SET CMP0169 OLD)
+endif()
 
 # Checkout remote repository
 macro(clone_repo name url tag)
@@ -14,24 +17,19 @@ macro(clone_repo name url tag)
 
     message(STATUS "Fetching ${name} ${${name_upper}_REPOSITORY} ${${name_upper}_TAG}")
 
-    # Check for FetchContent cmake support
-    if(${CMAKE_VERSION} VERSION_LESS "3.11")
-        message(FATAL_ERROR "CMake 3.11 required to fetch ${name}")
-    else()
-        include(FetchContent)
+    include(FetchContent)
 
-        FetchContent_Declare(${name}
-            GIT_REPOSITORY ${${name_upper}_REPOSITORY}
-            GIT_TAG ${${name_upper}_TAG}
-            SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/_deps/${name_lower}-src)
+    FetchContent_Declare(${name}
+        GIT_REPOSITORY ${${name_upper}_REPOSITORY}
+        GIT_TAG ${${name_upper}_TAG}
+        SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/_deps/${name_lower}-src)
 
-        FetchContent_GetProperties(${name} POPULATED ${name_lower}_POPULATED)
+    FetchContent_GetProperties(${name} POPULATED ${name_lower}_POPULATED)
 
-        if(NOT ${name_lower}_POPULATED)
-            FetchContent_Populate(${name})
-        endif()
-
-        set(${name_upper}_SOURCE_DIR ${${name_lower}_SOURCE_DIR})
-        set(${name_upper}_BINARY_DIR ${${name_lower}_BINARY_DIR})
+    if(NOT ${name_lower}_POPULATED)
+        FetchContent_Populate(${name})
     endif()
+
+    set(${name_upper}_SOURCE_DIR ${${name_lower}_SOURCE_DIR})
+    set(${name_upper}_BINARY_DIR ${${name_lower}_BINARY_DIR})
 endmacro()
